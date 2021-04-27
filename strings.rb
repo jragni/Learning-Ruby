49e429a075189fbb5976f0f149b73045c9d2ad9f
Jhensen Agni <jagni001@ucr.edu>
2021-04-26 18:47:07 -0700

    feat: First rb file

---
+++ strings.rb
@@ -0,0 +1,11 @@
+=begin 
+Write 3 expressions using each of string operations: + (concatenation), .length, and [] (indexing) Print out the result of your expressions using puts. Be sure to test your work by running your code!
+=end
+
+# Example:
+puts "Hello " + "World"
+
+# Write 3 more expressions below:
+puts "Hello".length
+puts "Hello"[2]
+puts "Jhensen " + "Agni"
